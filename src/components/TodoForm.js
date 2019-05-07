e5cf06800c1e3db88a3b3fc130f0aeacb79bb9a1
Adrianna <adrianna.juchacz@gmail.com>
2019-05-07 16:52:01 +0200

    created a firebase backend and added 'create todo'

---
+++ src/components/TodoForm.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+
+class TodoCreator extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Text:
+          <input value={this.props.todo.text} onChange={this.props.handler} name="text" type="text" className="form-control"/>
+        </label>
+        <label>
+          Progress:
+          <input value={this.props.todo.progress} onChange={this.props.handler} name="progress" type="text" className="form-control"/>
+        </label>
+        <h6>Set a deadline</h6>
+        <Calendar value={this.props.todo.deadline} onChange={this.props.dateHandler} />
+      </form>
+    );
+  }
+}
+export default TodoCreator;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,53 +1,113 @@
 import React from 'react';
-import TodoCreator from './TodoCreator';
+import TodoForm from './TodoForm';
 import TodoItem from './TodoItem';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+
+import firebase from '../config/firebase'
+import 'firebase/database'
 
 class Todos extends React.Component {
   constructor(props, context) {
     super(props, context);
 
+    this.database = firebase.database().ref().child('todos');
+
     this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
 
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCalendarChange = this.handleCalendarChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
     this.state = {
       show: false,
-      todos: [
-        {
-          id: 0,
-          text: 'First task',
-          deadline: '25-04-2019',
-          progress: '0%'
-        },
-        {
-          id: 1,
-          text: 'Second task',
-          deadline: '25-04-2019',
-          progress: '0%'
-        },
-        {
-          id: 2,
-          text: 'Third task',
-          deadline: '25-04-2019',
-          progress: '0%'
-        },
-        {
-          id: 3,
-          text: 'Fourth task',
-          deadline: '25-04-2019',
-          progress: '0%'
-        }
-      ]
+      todos: [],
+      formTodo: {
+        text: '',
+        progress: '',
+        deadline: new Date()
+      }
     };
+  }
+  componentWillMount(){
+    const previousTodos = this.state.todos;
+
+    // DataSnapshot
+    this.database.on('child_added', snap => {
+      previousTodos.push({
+        id: snap.key,
+        text: snap.val().text,
+        progress: snap.val().progress,
+        deadline: snap.val().deadline
+      })
+
+      this.setState({
+        todos: previousTodos
+      })
+    })
+
+    this.database.on('child_removed', snap => {
+      for(var i=0; i < previousTodos.length; i++){
+        if(previousTodos[i].id === snap.key){
+          previousTodos.splice(i, 1);
+        }
+      }
+
+      this.setState({
+        todos: previousTodos
+      })
+    })
+  }
+
+  addTodo(text) {
+    this.database.push().set({text: text})
   }
   handleClose() {
     this.setState({ show: false });
+    this.resetForm()
+  }
+
+  resetForm() {
+    var formTodo = {...this.state.formTodo}
+    Object.keys(formTodo).forEach( k => {
+      formTodo[k] = ''
+    })
+    formTodo['date'] = new Date()
+    this.setState({formTodo: formTodo})
   }
 
   handleShow() {
-    console.log('this.state.show', this.state)
     this.setState({ show: true });
+  }
+
+  handleCalendarChange(date) {
+    var formTodo = {...this.state.formTodo}
+    formTodo['date'] = date
+    this.setState({formTodo: formTodo})
+  }
+
+  handleChange(event) {
+    var formTodo = {...this.state.formTodo}
+    formTodo[event.target.name] = event.target.value
+    this.setState({formTodo: formTodo})
+  }
+
+  handleSubmit(event) {
+    var date = this.state.formTodo.deadline
+    var year = date.getFullYear().toString()
+    var month = date.getMonth().toString()
+    var day = date.getDate().toString()
+
+    date = day + '/' + month + '/' + year
+
+    event.preventDefault();
+    this.database.push().set({
+      text: this.state.formTodo.text,
+      progress: this.state.formTodo.progress,
+      deadline: date,
+    })
+    this.handleClose()
   }
 
   render() {
@@ -62,14 +122,14 @@ class Todos extends React.Component {
           </Modal.Header>
 
           <Modal.Body>
-            <TodoCreator/>
+            <TodoForm todo={this.state.formTodo} handler={this.handleChange} dateHandler={this.handleCalendarChange}/>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={this.handleClose} className="mt-5">
               Close
             </Button>
-            <Button variant="primary" onClick={this.handleClose} className="mt-5">
-              Create
+            <Button variant="primary" onClick={this.handleSubmit} className="mt-5">
+              Save
             </Button>
           </Modal.Footer>
         </Modal>
